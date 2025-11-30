de40e65acd38b128fad9f98d422cfe05fa96dcfd
Henry Robbins <hw.robbins@gmail.com>
2025-11-29 18:59:38 -0800

    Remove final validation run from test scripts

--- test_eoh.py
+++ test_eoh.py
@@ -2,7 +2,6 @@ import hydra
 import logging
 import os
 from pathlib import Path
-import subprocess
 from ga.eoh.config import Config
 from utils.evaluate import Evaluator
 from utils.llm_client.openai import OpenAIClient, OpenAIClientConfig
@@ -68,29 +67,6 @@ def main(cfg) -> None:
 
     best_code_overall, best_code_path_overall = llh.run()
     logging.info(f"Best Code Overall: {best_code_overall}")
-    best_path = best_code_path_overall.replace(".py", ".txt").replace(
-        "code", "response"
-    )
-    logging.info(
-        f"Best Code Path Overall: {print_hyperlink(best_path, best_code_path_overall)}"
-    )
-
-    # Run validation and redirect stdout to a file "best_code_overall_stdout.txt"
-    with open(f"{ROOT_DIR}/problems/{problem_name}/gpt.py", "w") as file:
-        file.writelines(best_code_overall + "\n")
-    test_script = f"{ROOT_DIR}/problems/{problem_name}/eval.py"
-    test_script_stdout = "best_code_overall_val_stdout.txt"
-    logging.info(f"Running validation script...: {print_hyperlink(test_script)}")
-    with open(test_script_stdout, "w") as stdout:
-        subprocess.run(["python", test_script, "-1", ROOT_DIR, "val"], stdout=stdout)
-    logging.info(
-        f"Validation script finished. Results are saved in {print_hyperlink(test_script_stdout)}."
-    )
-
-    # Print the results
-    with open(test_script_stdout, "r") as file:
-        for line in file.readlines():
-            logging.info(line.strip())
 
 
 if __name__ == "__main__":
--- test_reevo.py
+++ test_reevo.py
@@ -2,7 +2,6 @@ import hydra
 import logging
 import os
 from pathlib import Path
-import subprocess
 from utils.llm_client.openai import OpenAIClient, OpenAIClientConfig
 from utils.problem import ProblemPrompts
 from utils.utils import print_hyperlink
@@ -44,23 +43,6 @@ def main(cfg) -> None:
     logging.info(
         f"Best Code Path Overall: {print_hyperlink(best_path, best_code_path_overall)}"
     )
-
-    # Run validation and redirect stdout to a file "best_code_overall_stdout.txt"
-    with open(f"{ROOT_DIR}/problems/{problem_name}/gpt.py", "w") as file:
-        file.writelines(best_code_overall + "\n")
-    test_script = f"{ROOT_DIR}/problems/{problem_name}/eval.py"
-    test_script_stdout = "best_code_overall_val_stdout.txt"
-    logging.info(f"Running validation script...: {print_hyperlink(test_script)}")
-    with open(test_script_stdout, "w") as stdout:
-        subprocess.run(["python", test_script, "-1", ROOT_DIR, "val"], stdout=stdout)
-    logging.info(
-        f"Validation script finished. Results are saved in {print_hyperlink(test_script_stdout)}."
-    )
-
-    # Print the results
-    with open(test_script_stdout, "r") as file:
-        for line in file.readlines():
-            logging.info(line.strip())
 
 
 if __name__ == "__main__":
